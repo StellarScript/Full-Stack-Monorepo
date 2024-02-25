9f0e731738bbd92daa53a0abef424dd383fc6380
StellarScript <ej92@protonmail.com>
2024-02-24 18:33:20 -0800

    remove jest libs

--- apps/cdk/vitest.config.ts
+++ apps/cdk/vitest.config.ts
@@ -3,13 +3,11 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
    root: __dirname,
-   cacheDir: '../../node_modules/.vite/libs/data',
 
    plugins: [nxViteTsPaths()],
 
    test: {
       globals: true,
-      cache: { dir: '../../node_modules/.vitest' },
       environment: 'node',
       include: ['test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
       reporters: ['default'],
