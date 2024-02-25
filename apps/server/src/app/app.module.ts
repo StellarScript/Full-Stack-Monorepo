d6c16fa453a90e428335ece254de826b3f5498cb
StellarScript <ej92@protonmail.com>
2024-02-25 05:53:47 -0800

    server app module path

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { configuration } from '@appify/config';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
-import { UserModule } from '../modules/user/user.module';
+import { UserModule } from '@modules/user/user.module';
 
 @Module({
    imports: [ConfigModule.forRoot({ load: [configuration] }), UserModule],
