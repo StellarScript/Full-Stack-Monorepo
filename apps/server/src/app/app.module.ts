18cb335ee43873d5a28261af718600b2e646b080
StellarScript <ej92@protonmail.com>
2024-02-25 05:43:47 -0800

    server app setup

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { configuration } from '@appify/config';
 
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { UserModule } from '../modules/user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+   imports: [ConfigModule.forRoot({ load: [configuration] }), UserModule],
+   controllers: [AppController],
+   providers: [AppService],
 })
 export class AppModule {}
