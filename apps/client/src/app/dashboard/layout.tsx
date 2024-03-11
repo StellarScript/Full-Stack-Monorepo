71bbce4a282105a24ce2e724d4b8f4cc7c7370ab
StellarScript <ej92@protonmail.com>
2024-03-11 01:36:32 -0700

    dashboard layout

---
+++ apps/client/src/app/dashboard/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+   title: "Dashboard",
+   description: "User Dashboard",
+};
+
+export default function Layout({ children }: React.PropsWithChildren) {
+   return <div>{children}</div>;
+}
